fb0ddf4e2304994c007b9fca121c75d3d412519e
ollaidh <ollaidh@programmer.net>
2024-01-23 11:29:41 +0200

    Pass parameter by reference exercise

---
+++ yandex-belts/white-belt/2-2_1/2-2_1.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+void UpdateIfGreater(const int& first, int& second) {
+    if (first > second) {
+        second = first;
+    }
+}
+
+
+int main()
+{
+    return 0;
+}
