c0be3c05dc429ca9cdfa66f5899a27bac6ae7243
ollaidh <ollaidh@programmer.net>
2024-01-24 20:59:02 +0200

    values set

---
+++ yandex-belts/white-belt/2-6-2/2-6-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <map>
+
+std::set<std::string> BuildMapValuesSet(const std::map<int, std::string>& m) {
+    std::set<std::string> result;
+    for (const auto& [key, value] : m) {
+        result.insert(value);
+    }
+
+    return result;;
+
+}
