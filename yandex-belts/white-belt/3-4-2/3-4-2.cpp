b8c6bde7d7dba01769009aa4a532bc7a22ae5688
ollaidh <ollaidh@programmer.net>
2024-01-25 20:00:26 +0200

    class changing person name/surname and storing changes history

---
+++ yandex-belts/white-belt/3-4-2/3-4-2.cpp
@@ -0,0 +1,63 @@
+#include <functional>
+#include <iostream>
+#include <string>
+#include <map>
+
+class Person {
+public:
+  void ChangeFirstName(int year, const std::string& first_name) {
+    names[year] = first_name;
+  }
+
+  void ChangeLastName(int year, const std::string& last_name) {
+    lastNames[year] = last_name;
+  }
+
+  std::string GetFullName(int year) {
+    auto name = names.lower_bound(year);
+    auto surname = lastNames.lower_bound(year);
+
+    if (name == names.end() && surname == lastNames.end()) {
+      return "Incognito";
+    }
+
+    if (name == names.end()) {
+      return surname->second + " with unknown first name";
+    }
+
+    if (surname == lastNames.end()) {
+      return name->second + " with unknown last name";
+    }
+
+    auto upper = names.upper_bound(year);
+    auto lower = names.lower_bound(year);
+
+    return name->second + " " + surname->second;
+  }
+private:
+  std::map<int, std::string, std::greater<>> names;
+  std::map<int, std::string, std::greater<>> lastNames;
+
+};
+
+int main() {
+  Person person;
+
+  person.ChangeFirstName(1965, "Polina");
+  person.ChangeLastName(1967, "Sergeeva");
+  for (int year : {1900, 1965, 1990}) {
+    std::cout << person.GetFullName(year) << std::endl;
+  }
+
+  // person.ChangeFirstName(1970, "Appolinaria");
+  // for (int year : {1969, 1970}) {
+  //   std::cout << person.GetFullName(year) << std::endl;
+  // }
+
+  // person.ChangeLastName(1968, "Volkova");
+  // for (int year : {1969, 1970}) {
+  //   std::cout << person.GetFullName(year) << std::endl;
+  // }
+
+  return 0;
+}
