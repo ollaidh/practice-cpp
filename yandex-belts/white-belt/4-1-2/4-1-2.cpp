cb560c5177e7826e73e1c851b6541be1cceb7870
ollaidh <ollaidh@programmer.net>
2024-01-28 13:56:23 +0200

    cleanup

--- yandex-belts/white-belt/4-1-2/4-1-2.cpp
+++ yandex-belts/white-belt/4-1-2/4-1-2.cpp
@@ -3,17 +3,17 @@
 #include <vector>
 #include <map>
 
-// struct Image {
-//   double quality;
-//   double freshness;
-//   double rating;
-// };
+struct Image {
+  double quality;
+  double freshness;
+  double rating;
+};
 
-// struct Params {
-//   double a;
-//   double b;
-//   double c;
-// };
+struct Params {
+  double a;
+  double b;
+  double c;
+};
 
 class FunctionPart {
 public:
@@ -63,31 +63,31 @@ public:
   }
 };
 
-// Function MakeWeightFunction(const Params& params,
-//                             const Image& image) {
-//   Function function;
-//   function.AddPart('-', image.freshness * params.a + params.b);
-//   function.AddPart('+', image.rating * params.c);
-//   return function;
-// }
+Function MakeWeightFunction(const Params& params,
+                            const Image& image) {
+  Function function;
+  function.AddPart('-', image.freshness * params.a + params.b);
+  function.AddPart('+', image.rating * params.c);
+  return function;
+}
 
-// double ComputeImageWeight(const Params& params, const Image& image) {
-//   Function function = MakeWeightFunction(params, image);
-//   return function.Apply(image.quality);
-// }
+double ComputeImageWeight(const Params& params, const Image& image) {
+  Function function = MakeWeightFunction(params, image);
+  return function.Apply(image.quality);
+}
 
-// double ComputeQualityByWeight(const Params& params,
-//                               const Image& image,
-//                               double weight) {
-//   Function function = MakeWeightFunction(params, image);
-//   function.Invert();
-//   return function.Apply(weight);
-// }
+double ComputeQualityByWeight(const Params& params,
+                              const Image& image,
+                              double weight) {
+  Function function = MakeWeightFunction(params, image);
+  function.Invert();
+  return function.Apply(weight);
+}
 
-// int main() {
-//   Image image = {10, 2, 6};
-//   Params params = {4, 2, 6};
-//   std::cout << ComputeImageWeight(params, image) << std::endl;
-//   std::cout << ComputeQualityByWeight(params, image, 46) << std::endl;
-//   return 0;
-// }
+int main() {
+  Image image = {10, 2, 6};
+  Params params = {4, 2, 6};
+  std::cout << ComputeImageWeight(params, image) << std::endl;
+  std::cout << ComputeQualityByWeight(params, image, 46) << std::endl;
+  return 0;
+}
