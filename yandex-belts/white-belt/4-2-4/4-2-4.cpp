11d36cd2234452d33f6eb3c497689663267417cb
ollaidh <ollaidh@programmer.net>
2024-01-29 15:50:52 +0200

    upd output formatting

--- yandex-belts/white-belt/4-2-4/4-2-4.cpp
+++ yandex-belts/white-belt/4-2-4/4-2-4.cpp
@@ -9,24 +9,39 @@
 
 void outputTable(const std::string& inputPath, std::ostream& stream) {
   std::ifstream input(inputPath);
+  int nCols = 0;
+  int nRows = 0;
+
+  input >> nRows;
+  input.ignore(1);
+  input >> nCols;
+
+
   if (input) {
     std::string currLine;
     std::getline(input, currLine);
-    while (std::getline(input, currLine)) {
+    for (int i = 0; i < nRows; i++) {
+      std::getline(input, currLine);
       std::stringstream ss(currLine);
-      while(std::getline(ss, currLine, ',')){
+      for (int j = 0; j < nCols; j++) {
+        std::getline(ss, currLine, ',');
         stream << std::setw(10);
         stream << currLine;
+        if (j < nCols - 1) {
+          stream << " ";
+        }
       }
-      stream << std::endl;
+      if (i < nRows - 1) {
+        stream << std::endl;
+      }
     }
   }
 }
 
 
 int main() {
-  // const std::string inputPath = "input.txt";
-  const std::string inputPath = "/Users/maria/Documents/practice-cpp/practice-cpp/yandex-belts/white-belt/4-2-4/input.txt";
+  const std::string inputPath = "input.txt";
+  // const std::string inputPath = "/Users/maria/Documents/practice-cpp/practice-cpp/yandex-belts/white-belt/4-2-4/input.txt";
 
   outputTable(inputPath, std::cout);
 
