84c6fc87bc4297c26990bd579c56503b23a15536
ollaidh <ollaidh@programmer.net>
2024-02-02 15:52:43 +0200

    counting regions

---
+++ yandex-belts/yellow-belt/1-4-1/1-4-1.cpp
@@ -0,0 +1,107 @@
+#include <algorithm>
+#include <tuple>
+#include <map>
+#include <string>
+#include <iostream>
+
+#ifdef LOCAL_RUN
+
+enum class Lang {
+  DE, FR, IT
+};
+
+struct Region {
+  std::string std_name;
+  std::string parent_std_name;
+  std::map<Lang, std::string> names;
+  int64_t population;
+};
+
+#endif
+
+bool operator<(const Region& lhs, const Region& rhs) {
+    return std::tie(lhs.std_name, lhs.parent_std_name, lhs.names, lhs.population) <
+            std::tie(rhs.std_name, rhs.parent_std_name, rhs.names, rhs.population);
+}
+
+int FindMaxRepetitionCount(const std::vector<Region>& regions) {
+    if (regions.size() == 0) {
+        return 0;
+    }
+
+    std::map<Region, int> regCounter;
+    int maxReg;
+    for (const auto& region : regions) {
+        regCounter[region] += 1;
+        maxReg = std::max(maxReg, regCounter[region]);
+    }
+    return maxReg;
+}
+
+#ifdef LOCAL_RUN
+
+int main()
+{
+    std::cout << FindMaxRepetitionCount({
+      {
+          "Moscow",
+          "Russia",
+          {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+          89
+      }, {
+          "Russia",
+          "Eurasia",
+          {{Lang::DE, "Russland"}, {Lang::FR, "Russie"}, {Lang::IT, "Russia"}},
+          89
+      }, {
+          "Moscow",
+          "Russia",
+          {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+          89
+      }, {
+          "Moscow",
+          "Russia",
+          {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+          89
+      }, {
+          "Russia",
+          "Eurasia",
+          {{Lang::DE, "Russland"}, {Lang::FR, "Russie"}, {Lang::IT, "Russia"}},
+          89
+      },
+  }) << std::endl;
+
+  std::cout << FindMaxRepetitionCount({
+      {
+          "Moscow",
+          "Russia",
+          {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+          89
+      }, {
+          "Russia",
+          "Eurasia",
+          {{Lang::DE, "Russland"}, {Lang::FR, "Russie"}, {Lang::IT, "Russia"}},
+          89
+      }, {
+          "Moscow",
+          "Russia",
+          {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou deux"}, {Lang::IT, "Mosca"}},
+          89
+      }, {
+          "Moscow",
+          "Toulouse",
+          {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+          89
+      }, {
+          "Moscow",
+          "Russia",
+          {{Lang::DE, "Moskau"}, {Lang::FR, "Moscou"}, {Lang::IT, "Mosca"}},
+          31
+      },
+  }) << std::endl;
+
+  return 0;
+
+}
+
+#endif
