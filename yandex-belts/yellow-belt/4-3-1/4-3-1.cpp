907a8293ce5940857d9005213880b80736d9838d
ollaidh <ollaidh@programmer.net>
2024-02-12 15:26:18 +0200

    fixed earn

--- yandex-belts/yellow-belt/4-3-1/4-3-1.cpp
+++ yandex-belts/yellow-belt/4-3-1/4-3-1.cpp
@@ -1,10 +1,12 @@
 #include <algorithm>
+#include <exception>
 #include <iostream>
 #include <iterator>
 #include <numeric>
 #include <sstream>
 #include <vector>
 #include <string>
+#include <ctime>
 
 struct Date {
   Date(){};
@@ -23,7 +25,7 @@ int coundDaysDifference(Date startDate, Date endDate) {
     struct std::tm end = {0, 0, 0, endDate.day, endDate.month, endDate.year - 1900}; /* July 5, 2004 */
     std::time_t x = std::mktime(&start);
     std::time_t y = std::mktime(&end);
-    int difference = std::difftime(y, x) / (60 * 60 * 24);
+    int difference = std::difftime(y, x) / (60 * 60 * 24) + 1;
     return difference;
 }
 
@@ -38,18 +40,28 @@ public:
 
   void earn(const Date& startDate, const Date& endDate, double amount) {
     int periodDays = coundDaysDifference(startDate, endDate);
+    // std::cout << "Days: " << periodDays << "\n";
+
     double dailyIncome = amount / periodDays;
+    // std::cout << "Daily: " << dailyIncome << "\n";
 
-    std::vector<double>::iterator itStart = m_earnings.begin() + dateToIndex(startDate);
-    std::vector<double>::iterator itEnd = itStart + dateToIndex(endDate);
+    // std::vector<double>::iterator itStart = m_earnings.begin() + dateToIndex(startDate);
+    // std::vector<double>::iterator itEnd = m_earnings.begin() + dateToIndex(endDate);
+    // std::fill(itStart, itEnd + 1, dailyIncome);
 
-    std::fill(itStart, itEnd, dailyIncome);
+    for (int i = dateToIndex(startDate); i <= dateToIndex(endDate); i++) {
+      m_earnings[i] += dailyIncome;
+    }
   }
 
   double computeIncome(const Date& startDate, const Date& endDate) {
     std::vector<double>::iterator itStart = m_earnings.begin() + dateToIndex(startDate);
     std::vector<double>::iterator itEnd = itStart + dateToIndex(endDate);
     double result = std::accumulate(itStart, itEnd, 0);
+    // for (int i = dateToIndex(startDate); i < dateToIndex(endDate); i ++) {
+    //   std::cout << m_earnings[i] << " ";
+    // }
+    // std::cout << std::endl;
     return result;
   }
 
@@ -80,12 +92,17 @@ int main() {
   for (int i = 0; i < nActions; i++) {
     std::cin >> action;
     std::cin >> startDate;
-    std::cin >> endDate;
+    try {
+      std::cin >> endDate;
+    } catch (std::exception) {
+      endDate = startDate;
+    }
     if (action == "Earn") {
       std::cin >> amount;
       budget.earn(parseDate(startDate), parseDate(endDate), amount);
     } else if (action == "ComputeIncome") {
-      std::cout << budget.computeIncome(parseDate(startDate), parseDate(endDate));
+      std::cout.precision(25);
+      std::cout << budget.computeIncome(parseDate(startDate), parseDate(endDate)) << "\n";
     }
   }
 
