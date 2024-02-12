818b27409ec9d5abe85460b1b209077672c37035
ollaidh <ollaidh@programmer.net>
2024-02-12 12:34:52 +0200

    add converting date to vector index

--- yandex-belts/yellow-belt/4-3-1/4-3-1.cpp
+++ yandex-belts/yellow-belt/4-3-1/4-3-1.cpp
@@ -4,24 +4,50 @@
 #include <vector>
 #include <string>
 
-int dateToIndex(std::string);
+struct Date {
+  Date(int newYear, int newMonth, int newDay)
+  : year(newYear)
+  , month(newMonth)
+  , day(newDay)
+  {}
+  int year;
+  int month;
+  int day;
+};
+
+int dateToIndex(Date date) {
+    struct std::tm startDate = {0, 0, 0, 1, 1, 100};  // Starts from 01-01-2000
+    struct std::tm currDate = {0, 0, 0, date.day, date.month, date.year - 1900}; /* July 5, 2004 */
+    std::time_t x = std::mktime(&startDate);
+    std::time_t y = std::mktime(&currDate);
+    int difference = std::difftime(y, x) / (60 * 60 * 24);
+    return difference;
+}
 
 class Budget {
 public:
   Budget() : m_earnings(365){}
 
-  void earn(std::string startDate, std::string endDate, double amount);
-  double computeIncome(std::string startDate, std::string endDate);
+  void earn(Date startDate, Date endDate, double amount);
+  double computeIncome(Date startDate, Date endDate);
 
 private:
   std::vector<float> m_earnings;
 };
 
 int main() {
-  int nActions;
-  std::cin >> nActions;
+  // int nActions;
+  // std::cin >> nActions;
+  // for (int i = 0; i < nActions; i++) {
+  //   continue;
+  // }
 
-  for (int i = 0; i < nActions; i++) {
-    continue;
-  }
+  Date date(2000, 1,10);
+  int index = dateToIndex(date);
+  std::cout << index << "\n";
+
+
+
+
+
 }
