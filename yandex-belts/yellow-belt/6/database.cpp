472815a0bde0f867f2a56f8e98f53e7c76ac6f12
ollaidh <ollaidh@programmer.net>
2024-03-06 15:03:32 +0200

    add all tests for Database class methods

--- yandex-belts/yellow-belt/6/database.cpp
+++ yandex-belts/yellow-belt/6/database.cpp
@@ -75,10 +75,29 @@ void testDatabaseActions() {
     Assert(db.find(date1) != db.end(), "added date exists");
     AssertEqual(std::set<std::string>({"event1"}), db[date1], "right event for added date");
 
-    database.AddEvent(date1, "event2");
+    database.AddEvent(date1, "event11");
     db = database.getRecords();
     AssertEqual(1, db.size(), "still only date in db");
-    AssertEqual(std::set<std::string>({"event1", "event2"}), db[date1], "two events for one date");
+    AssertEqual(std::set<std::string>({"event1", "event11"}), db[date1], "two events for one date");
+
+    AssertEqual(database.Find(date1), std::set<std::string>({"event1", "event11"}), "Find method test");
+
+    Date date2 = Date(2021, 12, 11);
+    database.AddEvent(date2, "event2");
+    database.AddEvent(date2, "event22");
+    database.AddEvent(date2, "event222");
+    Date date3 = Date(2022, 12, 21);
+
+    AssertEqual(database.Find(date2), std::set<std::string>({"event2", "event22", "event222"}),
+                "check date exists with right events");
+    database.DeleteEvent(date2, "event22");
+    db = database.getRecords();
+    AssertEqual(std::set<std::string>({"event2", "event222"}), db[date2], "Delete event test");
+
+    database.DeleteDate(date2);
+    db = database.getRecords();
+    Assert(db.find(date2) == db.end(), "Delete date test");
+
 }
 
 
