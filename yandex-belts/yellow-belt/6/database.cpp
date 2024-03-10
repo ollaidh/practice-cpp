2756eda3cba7d227bbd602abd03b2beb7ce28b0f
ollaidh <ollaidh@programmer.net>
2024-03-10 16:11:34 +0200

    add Entry struct for printing out results of find

--- yandex-belts/yellow-belt/6/database.cpp
+++ yandex-belts/yellow-belt/6/database.cpp
@@ -9,6 +9,17 @@
 #include "condition_parser.h"
 #endif
 
+
+std::ostream& operator<<(std::ostream& stream, const Entry& entry) {
+  stream << entry.date;
+  stream << " ";
+  stream << entry.event;
+  stream << std::endl;
+
+  return stream;
+}
+
+
 // add event for specific date
 void Database::Add(const Date& date, const std::string& event) {
     m_db[date].insert(event);
@@ -78,6 +89,7 @@ void testDatabaseActions() {
     AssertEqual(std::set<std::string>({"event2", "event222"}), db[date2], "Delete event test");
     AssertEqual(1, nRemoved, "Number of removed records");
 
+    // Delete all records for date 2021-12-11 and the date becomes empty and is deleted automatically
     line = "date == 2021-12-11";
     istringstream is2(line);
     auto condition2 = ParseCondition(is2);
--- yandex-belts/yellow-belt/6/database.h
+++ yandex-belts/yellow-belt/6/database.h
@@ -6,12 +6,26 @@
 
 #include "date.h"
 
+struct Entry {
+    Entry(const Date& new_date, const std::string& new_event)
+    : date(new_date)
+    , event(new_event)
+    {}
+
+    Date date;
+    std::string event;
+};
+
+std::ostream& operator<<(std::ostream& stream, const Entry& entry);
+
+
 class Database {
 public:
     // add event for specific date
     void Add(const Date& date, const std::string& event);
 
-    // delete record if condition is true
+    // delete all records for which condition is true,
+    // count number of deleted
     template<typename Predicate>
     int RemoveIf(Predicate predicate) {
         auto curr_db = m_db;
@@ -30,6 +44,25 @@ public:
         m_db = curr_db;
         return nDeleted;
     }
+
+    // find all records for which condition is true
+    // template<typename Predicate>
+    // int FindIf(Predicate predicate) {
+    //     int nDeleted = 0;
+    //     for (auto& [date, events] : m_db) {
+    //         for (const auto &event : events) {
+    //             if (predicate(date, event)) {
+    //                 curr_db[date].erase(event);
+    //                 if (curr_db[date].empty()) {
+    //                     curr_db.erase(date);
+    //                 }
+    //                 nDeleted += 1;
+    //             }
+    //         }
+    //     }
+    //     m_db = curr_db;
+    //     return nDeleted;
+    // }
 
     // delete all events for specific date and date itself
     int DeleteDate(const Date& date);
